78043ea3848c7cbb7bf96d8f0041198ce131d15c
Youssef Ben Attar <youssefdisvitas@gmail.com>
2024-04-13 00:37:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sqflite_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sqflite_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sqflite_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
